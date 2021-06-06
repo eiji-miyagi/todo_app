c7df2a824a8e07f58167c5d112e032cd43fca553
宮城　永次 <miyagieiji@miyagieijinoMacBook-Pro.local>
2021-06-06 14:47:37 +0900

    add auth check

--- todo_app/app/Http/Controllers/HomeController.php
+++ todo_app/app/Http/Controllers/HomeController.php
@@ -13,6 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
+        // HomeController.phpが呼び出された際にユーザーがログイン済みかどうかをチェックしています。
         $this->middleware('auth');
     }
 
